def1600873c197f1d34f9647dbf2a22a7da29241
thiago dos santos martins <thiago.ds.martins@tutanota.com>
2023-11-30 01:13:54 -0300

    movida alert component

---
+++ lib/custom_alert_box.dart
@@ -0,0 +1,36 @@
+library custom_alert_box;
+
+import 'package:flutter/material.dart';
+
+class CustomAlertBox {
+  static Future showCustomAlertBox({
+    required BuildContext context,
+    required Widget willDisplayWidget,
+  }) {
+    assert(context != null, "context is null!!");
+    assert(willDisplayWidget != null, "willDisplayWidget is null!!");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: const Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
+}
